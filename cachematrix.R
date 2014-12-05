4f2a57f7c609688cc32248339fe38d4a75144788
rafamonge <rafamonge@gmail.com>
2014-12-05 10:57:07 -0600

    implemented!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+#SampleMatrix A & B
+A = matrix( 
+     c(1,0,1,1), # the data elements 
+     nrow=2,              # number of rows 
+     ncol=2,              # number of columns 
+     byrow = TRUE)        # fill matrix by rows 
 
-## Write a short comment describing this function
+B = matrix( 
+  c(1,0,0,0,1,0,0,1,1), # the data elements 
+  nrow=3,              # number of rows 
+  ncol=3,              # number of columns 
+  byrow = TRUE)        # fill matrix by rows 
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverseMatrix <- NULL
+  set <- function(newMatrix){
+    x<<-newMatrix
+    inverseMatrix <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(newInverse) inverseMatrix <<- newInverse
+  getInverse <- function() inverseMatrix
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data,...)
+  x$setInverse(inverse)
+  inverse
 }
