69f526eb70fe7b73f82f5b8f7e36a509d6b7fe9e
rafamonge <rafamonge@gmail.com>
2014-12-05 12:50:08 -0600

    Added documentation
    Added documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,8 @@
-#SampleMatrix A & B
-A = matrix( 
-     c(1,0,1,1), # the data elements 
-     nrow=2,              # number of rows 
-     ncol=2,              # number of columns 
-     byrow = TRUE)        # fill matrix by rows 
-
-B = matrix( 
-  c(1,0,0,0,1,0,0,1,1), # the data elements 
-  nrow=3,              # number of rows 
-  ncol=3,              # number of columns 
-  byrow = TRUE)        # fill matrix by rows 
-
+#Returns: List with the following items
+#set: set the value of the matrix
+#get: get the value of the matrix
+#setInverse: set the value of the inverse of the matrix
+#getInverse: get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   inverseMatrix <- NULL
@@ -27,6 +19,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+#Description: Calculates the inverseMatrix for a makeCacheMatrix object. If the object already has the inverse in a cache, it returns the cached inverse.
+#Input:
+#x: An object of type makeCacheMatrix
+#Returns: the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
   inverse <- x$getInverse()
@@ -39,3 +35,17 @@ cacheSolve <- function(x, ...) {
   x$setInverse(inverse)
   inverse
 }
+
+
+# 2 sample invertible matrixes so you can test the code below
+A = matrix( 
+  c(1,0,1,1), # the data elements 
+  nrow=2,              # number of rows 
+  ncol=2,              # number of columns 
+  byrow = TRUE)        # fill matrix by rows 
+
+B = matrix( 
+  c(1,0,0,0,1,0,0,1,1), # the data elements 
+  nrow=3,              # number of rows 
+  ncol=3,              # number of columns 
+  byrow = TRUE)        # fill matrix by rows 
